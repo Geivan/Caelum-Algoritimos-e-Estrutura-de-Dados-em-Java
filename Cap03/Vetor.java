b97f8715603177cddc059d75e8ab158dc61e8e14
Geivan Martins <47045921+Geivan@users.noreply.github.com>
2023-02-22 23:40:24 -0300

    .

---
+++ Cap03/Vetor.java
@@ -0,0 +1,33 @@
+public class Vetor {
+
+    // Declarando e Inicializando um array de Aluno com capacidade para 100.
+    private Aluno[] alunos = new Aluno[100];
+
+    public void adiciona (Aluno aluno) {
+        // implementação
+    }
+
+    public void adiciona(int posicao, Aluno aluno) {
+        // implementação
+    }
+
+    public Aluno pega(int posicao) {
+        // implementação
+    }
+
+    public void remove(int posicao) {
+        // implementação
+    }
+
+    public boolean contem(Aluno aluno) {
+        // implementação
+    }
+
+    public int tamanho() {
+        // implementação
+    }
+
+    public String toString() {
+        return Arrays.toString(alunos);
+    }
+}
